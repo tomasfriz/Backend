4c436d466ea5b113b58e919de247c6773a91e9f9
tomasfriz <tomyagustinf@gmail.com>
2022-08-12 11:56:04 -0300

    base de spring boot

---
+++ Proyecto-Final/Back-end/src/main/java/com/portfolio/TomasAgustinFriz/TomasAgustinFrizApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.TomasAgustinFriz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomasAgustinFrizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomasAgustinFrizApplication.class, args);
+	}
+
+}
