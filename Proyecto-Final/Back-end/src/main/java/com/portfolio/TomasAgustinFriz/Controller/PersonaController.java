4410a20c9ee7ce42d291aff8290cfde3402ac15d
tomasfriz <tomyagustinf@gmail.com>
2022-08-15 15:59:19 -0300

    vinculacion con el back-end con el front-end

--- Proyecto-Final/Back-end/src/main/java/com/portfolio/TomasAgustinFriz/Controller/PersonaController.java
+++ Proyecto-Final/Back-end/src/main/java/com/portfolio/TomasAgustinFriz/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.TomasAgustinFriz.Entity.Persona;
 import com.portfolio.TomasAgustinFriz.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -52,5 +54,10 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
