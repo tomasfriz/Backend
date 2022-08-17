6420ea04f0ea5f4512c8b06b8d6293095dcb825b
tomasfriz <tomyagustinf@gmail.com>
2022-08-17 18:30:52 -0300

    corregido la carga de datos en postman

--- Proyecto-Final/Back-end/src/main/java/com/portfolio/TomasAgustinFriz/Controller/PersonaController.java
+++ Proyecto-Final/Back-end/src/main/java/com/portfolio/TomasAgustinFriz/Controller/PersonaController.java
@@ -58,6 +58,6 @@ public class PersonaController {
     
     @GetMapping("/personas/traer/perfil")
     public Persona findPersona(){
-        return ipersonaService.findPersona((long)1);
+        return ipersonaService.findPersona((long)3);
     }
 }
